dfada7c449da33d7549a405582c075c9e688e49c
dmitryk <dmitryk@iterion.tu>
2018-05-25 18:01:43 +0300

    init commit

---
+++ landing/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=128)
+    project = models.CharField(max_length=128)
+    status = models.CharField(max_length=128)
+    implementer = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
---
+++ landing/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import TaskForm
+
+
+def landing(request):
+    name = "Dmitryk"
+    form = TaskForm(request.POST or None)
+
+    if request.method == "POST":
+        print(form)
+
+        form_new = form.save()
+
+    return render(request, 'landing/landing.html', locals())
